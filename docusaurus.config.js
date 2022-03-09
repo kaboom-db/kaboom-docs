06d2aa586a0efd95d45acd8f3045a25ed42ba269
Tanveer Najib <tanveernajib@gmail.com>
2022-03-09 12:13:42 +0000

    update footer

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -96,7 +96,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Kaboom DB.`,
       },
       prism: {
         theme: lightCodeTheme,
